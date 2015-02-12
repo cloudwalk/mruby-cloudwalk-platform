795f78a478d618c666080f7937114bd2990a4bcd
Thiago Scalone <thiago@scalone.com.br>
2015-02-12 17:21:32 -0200

    Simplify System and Audio methods.

--- src/system.c
+++ src/system.c
@@ -70,10 +70,10 @@ mrb_system_init(mrb_state* mrb)
   audio = mrb_define_class(mrb, "Audio", platform);
   system = mrb_define_class(mrb, "System", platform);
 
-  mrb_define_class_method(mrb , system , "_serial"     , mrb_system_s__serial        , MRB_ARGS_NONE());
-  mrb_define_class_method(mrb , system , "_backlight=" , mrb_system_s__set_backlight , MRB_ARGS_REQ(1));
-  mrb_define_class_method(mrb , system , "_battery"    , mrb_system_s__battery       , MRB_ARGS_NONE());
-  mrb_define_class_method(mrb , audio  , "_beep"       , mrb_audio_s__beep           , MRB_ARGS_REQ(2));
-  mrb_define_class_method(mrb , system , "_reboot"     , mrb_system_s_reboot         , MRB_ARGS_NONE());
+  mrb_define_class_method(mrb , system , "serial"     , mrb_system_s__serial        , MRB_ARGS_NONE());
+  mrb_define_class_method(mrb , system , "backlight=" , mrb_system_s__set_backlight , MRB_ARGS_REQ(1));
+  mrb_define_class_method(mrb , system , "battery"    , mrb_system_s__battery       , MRB_ARGS_NONE());
+  mrb_define_class_method(mrb , audio  , "beep"       , mrb_audio_s__beep           , MRB_ARGS_REQ(2));
+  mrb_define_class_method(mrb , system , "reboot"     , mrb_system_s_reboot         , MRB_ARGS_NONE());
 }
 
