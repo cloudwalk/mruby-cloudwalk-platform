288799702b46ae416c11ffc865aefc356656d1bb
Thiago Scalone <thiago@scalone.com.br>
2015-10-24 19:43:56 -0200

    Extract IO ruby code from platform.rb and create a file for it.

--- mrblib/platform.rb
+++ mrblib/platform.rb
@@ -1,18 +1,9 @@
 class Platform
   Network = ::Network if Kernel.const_defined? :Network
-  IO      = ::IO
 
   class System
     class << self
       alias_method :restart, :reboot if self.respond_to? :reboot
-    end
-  end
-
-  class IO
-    DEFAULT_TIMEOUT = 30000
-
-    class << self
-      attr_accessor :timeout
     end
   end
 
