8be1a978d2d8d01c6b97c36416084d3d3dcb8417
Thiago Scalone <thiago@scalone.com.br>
2015-10-24 19:45:46 -0200

    Add cloudwalk_handshake load and configuration on setup.

--- mrblib/platform.rb
+++ mrblib/platform.rb
@@ -1,6 +1,13 @@
 class Platform
   # Will be called at RunTime boot
   def self.setup
+    begin
+      require 'cloudwalk_handshake'
+      CloudwalkHandshake.configure!
+    rescue LoadError
+    rescue NameError
+    end
     # TODO Implement
   end
 end
+
