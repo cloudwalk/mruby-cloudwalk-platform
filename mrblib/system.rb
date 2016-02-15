c5b486361eeee2607c8b64936bea775a06e0e576
Thiago Scalone <thiago@scalone.com.br>
2016-02-15 18:24:23 -0200

    Add System.brand/model/os_version/sdk_version/ pinpad_version/versions(all).

--- mrblib/system.rb
+++ mrblib/system.rb
@@ -3,6 +3,15 @@ class Platform
     class << self
       alias_method :restart, :reboot if self.respond_to? :reboot
     end
+
+    def self.versions
+      hash = Hash.new
+      hash["OS"]     = self.os_version
+      hash["SDK"]    = self.sdk_version
+      hash["EMV"]    = Platform::EMV.version if Platform.const_defined? :EMV
+      hash["Pinpad"] = self.pinpad_version
+      hash
+    end
   end
 end
 
--- src/system.c
+++ src/system.c
@@ -89,6 +89,67 @@ mrb_system_s_hwclock(mrb_state *mrb, mrb_value self)
   mrb_fixnum_value(0);
 }
 
+static mrb_value
+mrb_system_s_model(mrb_state *mrb, mrb_value self)
+{
+  char version[32]="\0";
+
+  memset(&version, 0, sizeof(version));
+
+  /*TODO Implement*/
+  /*OsGetSysVer(TYPE_PED_VER, version);*/
+
+  return mrb_str_new_cstr(mrb, version);
+}
+
+static mrb_value
+mrb_system_s_brand(mrb_state *mrb, mrb_value self)
+{
+  char brand[32]="\0";
+
+  memset(&brand, 0, sizeof(brand));
+
+  /*TODO Implement*/
+
+  return mrb_str_new_cstr(mrb, brand);
+}
+
+static mrb_value
+mrb_system_s_os_version(mrb_state *mrb, mrb_value self)
+{
+  char version[32]="\0";
+
+  memset(&version, 0, sizeof(version));
+
+  /*TODO Implement*/
+
+  return mrb_str_new_cstr(mrb, version);
+}
+
+static mrb_value
+mrb_system_s_sdk_version(mrb_state *mrb, mrb_value self)
+{
+  char version[32]="\0";
+
+  memset(&version, 0, sizeof(version));
+
+  /*TODO Implement*/
+
+  return mrb_str_new_cstr(mrb, version);
+}
+
+static mrb_value
+mrb_system_s_pinpad_version(mrb_state *mrb, mrb_value self)
+{
+  char version[32]="\0";
+
+  memset(&version, 0, sizeof(version));
+
+  /*TODO Implement*/
+
+  return mrb_str_new_cstr(mrb, version);
+}
+
 void
 mrb_system_init(mrb_state* mrb)
 {
@@ -100,11 +161,16 @@ mrb_system_init(mrb_state* mrb)
   audio    = mrb_define_class_under(mrb, platform, "Audio", mrb->object_class);
   system   = mrb_define_class_under(mrb, platform, "System", mrb->object_class);
 
-  mrb_define_class_method(mrb , system , "serial"     , mrb_system_s__serial        , MRB_ARGS_NONE());
-  mrb_define_class_method(mrb , system , "backlight=" , mrb_system_s__set_backlight , MRB_ARGS_REQ(1));
-  mrb_define_class_method(mrb , system , "battery"    , mrb_system_s__battery       , MRB_ARGS_NONE());
-  mrb_define_class_method(mrb , audio  , "beep"       , mrb_audio_s__beep           , MRB_ARGS_REQ(2));
-  mrb_define_class_method(mrb , system , "reboot"     , mrb_system_s_reboot         , MRB_ARGS_NONE());
-  mrb_define_class_method(mrb , system , "hwclock"    , mrb_system_s_hwclock        , MRB_ARGS_REQ(6));
+  mrb_define_class_method(mrb , system , "serial"         , mrb_system_s__serial        , MRB_ARGS_NONE());
+  mrb_define_class_method(mrb , system , "backlight="     , mrb_system_s__set_backlight , MRB_ARGS_REQ(1));
+  mrb_define_class_method(mrb , system , "battery"        , mrb_system_s__battery       , MRB_ARGS_NONE());
+  mrb_define_class_method(mrb , audio  , "beep"           , mrb_audio_s__beep           , MRB_ARGS_REQ(2));
+  mrb_define_class_method(mrb , system , "reboot"         , mrb_system_s_reboot         , MRB_ARGS_NONE());
+  mrb_define_class_method(mrb , system , "hwclock"        , mrb_system_s_hwclock        , MRB_ARGS_REQ(6));
+  mrb_define_class_method(mrb , system , "model"          , mrb_system_s_model          , MRB_ARGS_NONE());
+  mrb_define_class_method(mrb , system , "brand"          , mrb_system_s_brand          , MRB_ARGS_NONE());
+  mrb_define_class_method(mrb , system , "os_version"     , mrb_system_s_os_version     , MRB_ARGS_NONE());
+  mrb_define_class_method(mrb , system , "sdk_version"    , mrb_system_s_sdk_version    , MRB_ARGS_NONE());
+  mrb_define_class_method(mrb , system , "pinpad_version" , mrb_system_s_pinpad_version , MRB_ARGS_NONE());
 }
 
