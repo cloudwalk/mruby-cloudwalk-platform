9793a2d9a80ec228fe1edfb444c2ebb0c06547ca
Thiago Scalone <thiago@scalone.com.br>
2016-03-30 17:00:06 -0300

    Add binds for status bar.

--- src/display.c
+++ src/display.c
@@ -53,6 +53,36 @@ mrb_display_s_print_line(mrb_state *mrb, mrb_value self)
   return mrb_nil_value();
 }
 
+mrb_value
+mrb_display_s_print_status_bar(mrb_state *mrb, mrb_value self)
+{
+  mrb_value path;
+  mrb_int slot = 0;
+
+  mrb_get_args(mrb, "io", &slot, &path);
+
+  /*TODO Implement*/
+  /*if (mrb_string_p(path)) {*/
+    /*if (XuiSetStatusbarIcon(slot, RSTRING_PTR(path)) == 0)*/
+      /*return mrb_true_value();*/
+    /*else*/
+      /*return mrb_nil_value();*/
+  /*} else {*/
+    /*if (XuiSetStatusbarIcon(slot, NULL) == 0)*/
+      /*return mrb_true_value();*/
+    /*else*/
+      /*return mrb_nil_value();*/
+    /*return mrb_nil_value();*/
+  /*}*/
+  return mrb_nil_value();
+}
+
+mrb_value
+mrb_display_s_status_bar_slots_available(mrb_state *mrb, mrb_value self)
+{
+  return mrb_fixnum_value(8);
+}
+
 void
 mrb_display_init(mrb_state* mrb)
 {
@@ -62,8 +92,10 @@ mrb_display_init(mrb_state* mrb)
   platform = mrb_class_get(mrb, "Platform");
   display = mrb_define_class_under(mrb, platform, "Display", mrb->object_class);
 
-  mrb_define_class_method(mrb , display , "clear"        , mrb_display_s_clear       , MRB_ARGS_NONE());
-  mrb_define_class_method(mrb , display , "clear_line"   , mrb_display_s_clear_line  , MRB_ARGS_REQ(1));
-  mrb_define_class_method(mrb , display , "print_bitmap" , mrb_display_s_print_bitmap , MRB_ARGS_REQ(3));
-  mrb_define_class_method(mrb , display , "print_line"   , mrb_display_s_print_line   , MRB_ARGS_REQ(3));
+  mrb_define_class_method(mrb , display , "clear"                      , mrb_display_s_clear                      , MRB_ARGS_NONE());
+  mrb_define_class_method(mrb , display , "clear_line"                 , mrb_display_s_clear_line                 , MRB_ARGS_REQ(1));
+  mrb_define_class_method(mrb , display , "print_bitmap"               , mrb_display_s_print_bitmap               , MRB_ARGS_REQ(3));
+  mrb_define_class_method(mrb , display , "print_line"                 , mrb_display_s_print_line                 , MRB_ARGS_REQ(3));
+  mrb_define_class_method(mrb , display , "print_status_bar"           , mrb_display_s_print_status_bar           , MRB_ARGS_REQ(2));
+  mrb_define_class_method(mrb , display , "status_bar_slots_available" , mrb_display_s_status_bar_slots_available , MRB_ARGS_NONE());
 }
